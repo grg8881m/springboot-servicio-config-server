324909fe5a18951850aa1775eb83620a2aa2818b
Harold Adrian Bolaños Rodriguez <habr0075@coomeva.com.co>
2020-04-09 18:48:06 -0500

    Implementacion de la primera configuracion que se tiene

---
+++ src/test/java/com/formaciondbi/springboot/app/config/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formaciondbi.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
